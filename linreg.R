b31693931768e86c8ac384074401e7983b22b58e
roli1920 <karczubroland@gmail.com>
2024-03-06 15:04:37 +0100

    fixed linReg

--- linreg.R
+++ linreg.R
@@ -1,12 +1,15 @@
 linreg <- function(X,Y)
 {
 	plot(X,Y);
+
 	sumA = 0;
 	sumB = 0;
 	sumC = 0;
+
 	sumASquare = 0;
 	detAFirst = 0;
 	detASecond = 0;
+
 	a = 0;
 	for(i in 1:length(X))
 	{
@@ -16,17 +19,23 @@ linreg <- function(X,Y)
 		sumASquare = sumASquare + X[i]^2;
 	}
 	sumB = -sumB;
+	
 	detAFirst = sumA * length(X) - sumC * sumB;
 	detASecond = sumASquare * length(X) - (-sumB) * sumB;
 	a = detAFirst / detASecond;
 
-	detBFirst = -sumB * sumC - (-sumB) * sumA;
+	detBFirst = sumASquare * sumC - (-sumB) * sumA;
 	detBSecond = sumASquare * length(X) - (-sumB) * sumB;
 	b = detBFirst / detBSecond;
 	
 	lines(X, a*X+b, type = "l", lty = 1, col="blue")
+	
+	print(a);
+	print(b);
+	
 }
 
-X = rnorm(300);
-Y = rnorm(300);
+X = c(1,2,3,4,5);
+Y = c(3.1,5.1,6.9,9.1,10.1);
+
 linreg(X,Y);
