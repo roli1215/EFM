8dae5ba7f9e66a4edd915698f16daff8abdcc77e
roli1920 <karczubroland@gmail.com>
2024-03-20 14:35:32 +0100

    full done polynomReg

--- polynomReg.R
+++ polynomReg.R
@@ -1,19 +1,27 @@
-X <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-Y <- 5 * X^2 + 3 * X + 7
+X <- c(1, 2, 3, 4)
+Y <- 5 * X^2 + 3 * X + 2
 n <- length(X)
 
 PolynomialReg <- function(X, Y) {
-    detUpper <- detUpperCalculate(X, Y)
+    detUpperA0 <- detUpperCalculateA0(X, Y)
+    detUpperA1 <-detUpperCalculateA1(X,Y)
+    detUpperA2 <-detUpperCalculateA2(X,Y)
     detBottom <- detBottomCalculate(X, Y)
 
-    A0 <- detUpper / detBottom
-    A1 <- Summary(X)
-    A2sum <- n * (X^3)
-    A2 <- Summary(A2sum)
+    
+
+    A0 <- detUpperA0 / detBottom
+    A1 <- detUpperA1 / detBottom
+    A2 <- detUpperA2 / detBottom
 
     plot(X, Y)
 
     lines(X, A2 * X^2 + A1 * X + A0, type = "l", lty = 1, col = "red")
+
+    print(A0)
+    print(A1)
+    print(A2)
+    print(Y)
 }
 
 detBottomCalculate <- function(X, Y) {
@@ -29,7 +37,7 @@ detBottomCalculate <- function(X, Y) {
     return(detBottom)
 }
 
-detUpperCalculate <- function(X, Y) {
+detUpperCalculateA0 <- function(X, Y) {
     sumx4 <- Summary(X^4)
     sumx3 <- Summary(X^3)
     sumx2y <- Summary(X^2, Y)
@@ -41,6 +49,36 @@ detUpperCalculate <- function(X, Y) {
     detUpper <- sumx4 * CalcDet(sumx2, sumxy, sumx, sumy) -
         sumx3 * CalcDet(sumx3, sumxy, sumx2, sumy) +
         sumx2y * CalcDet(sumx3, sumx2, sumx2, sumx)
+
+    return(detUpper)
+}
+detUpperCalculateA1 <- function(X, Y) {
+    sumx4 <- Summary(X^4)
+    sumx3 <- Summary(X^3)
+    sumx2y <- Summary(X^2, Y)
+    sumx2 <- Summary(X^2)
+    sumxy <- Summary(X, Y)
+    sumx <- Summary(X)
+    sumy <- Summary(Y)
+
+    detUpper <- sumx4 * CalcDet(sumxy, sumx, sumy, n) -
+        sumx2y * CalcDet(sumx3, sumx, sumx2, n) +
+        sumx2 * CalcDet(sumx3, sumxy, sumx2, sumy)
+
+    return(detUpper)
+}
+detUpperCalculateA2 <- function(X, Y) {
+    sumx4 <- Summary(X^4)
+    sumx3 <- Summary(X^3)
+    sumx2y <- Summary(X^2, Y)
+    sumx2 <- Summary(X^2)
+    sumxy <- Summary(X, Y)
+    sumx <- Summary(X)
+    sumy <- Summary(Y)
+
+    detUpper <- sumx2y * CalcDet(sumx2, sumx, sumx, n) -
+        sumx3 * CalcDet(sumxy, sumx, sumy, n) +
+        sumx2 * CalcDet(sumxy, sumx2, sumy, sumx)
 
     return(detUpper)
 }
